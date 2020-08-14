1b078c399bd19a77a837a9373a423fbf67af2119
Ali Özkan <aliozzkan@gmail.com>
2020-08-15 02:53:11 +0300

    Modify Card Item

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { Container } from './partials';
+import React, { useState } from 'react';
+import { useMounted, useFetchData } from '@/hooks';
+import { PokemonListService } from '@/services';
+
+import { Input, ActivityIndicator } from '@/components';
+import { Container, GridCard, Card } from './partials';
 
 function Home() {
+  const [pokemons, setPokemons] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pokemonsFetch, mutationPokemonsFetch] = useFetchData();
+
+  useMounted(() => {
+    PokemonListService(mutationPokemonsFetch, {
+      onSuccess: (resp) => {
+        setPokemons([...resp.results]);
+      },
+    })();
+  });
+
+  const cardRows = [];
+  function mapPokemon(pokemon, key) {
+    cardRows.push(<Card key={key} name={pokemon.name} />);
+  }
+
+  if (searchQuery !== '') {
+    pokemons
+      .filter((pokemon) => pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()))
+      .forEach(mapPokemon);
+  } else {
+    pokemons.forEach(mapPokemon);
+  }
+
   return (
     <div>
       <Container>
         <div className="logo">Logo</div>
-        <div className="grid-card-container">
-          <div className="card">Card A</div>
-          <div className="card">Card A</div>
-          <div className="card">Card A</div>
-          <div className="card">Card A</div>
-          <div className="card">Card A</div>
-          <div className="card">Card A</div>
-        </div>
+        {pokemonsFetch.isFulfilled ? (
+          <>
+            <div className="flex justify-center m-5">
+              <Input
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search"
+              />
+            </div>
+            <GridCard>{cardRows}</GridCard>
+          </>
+        ) : (
+          <ActivityIndicator />
+        )}
       </Container>
     </div>
   );
--- src/pages/Home/partials/index.js
+++ src/pages/Home/partials/index.js
@@ -1,1 +1,3 @@
 export { default as Container } from './Container';
+export { default as GridCard } from './GridCard';
+export { default as Card } from './Card';
