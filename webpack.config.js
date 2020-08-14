90c1483c6e758b030c1663cf153caa3e98286a52
Ali Özkan <aliozzkan@gmail.com>
2020-08-14 19:22:04 +0300

    Eslint Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/App.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       // Rules to load scss
       {
-      // Some change here
+        // Some change here
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['eslint-loader'],
+      },
+    ],
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+    contentBase: path.join(__dirname, 'public'),
+  },
 };
