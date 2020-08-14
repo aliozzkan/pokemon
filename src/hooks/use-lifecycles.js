8ab986cf6cb933a75c6c3499c6b0abfc4ecc6c81
Ali Özkan <aliozzkan@gmail.com>
2020-08-15 01:13:03 +0300

    Created Hooks Lifecycle Methods For Function Comp

---
+++ src/hooks/use-lifecycles.js
@@ -0,0 +1,53 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
+
+export function useMounted(func) {
+  const [_mounted, _setMounted] = useState();
+  const [_unMounted, _setUnmounted] = useState();
+
+  useEffect(() => {
+    if (!_mounted && !_unMounted) {
+      func();
+      _setMounted(true);
+    }
+    return () => {
+      _setUnmounted(true);
+    };
+  });
+}
+export function beforeMount(func) {
+  const [_mounted, _setMounted] = useState();
+  const [_unMounted, _setUnmounted] = useState();
+
+  useLayoutEffect(() => {
+    if (!_mounted && !_unMounted) {
+      func();
+      _setMounted(true);
+    }
+    return () => {
+      _setUnmounted(true);
+    };
+  });
+}
+
+export function useUnmounted(func) {
+  useEffect(() => {
+    return () => {
+      func();
+    };
+  });
+}
+
+export function useUpdated(func, arr = undefined) {
+  const [_mounted, _setMounted] = useState();
+  const [_unMounted, _setUnmounted] = useState();
+
+  useEffect(() => {
+    if (!_mounted && !_unMounted) {
+      func();
+      _setMounted(true);
+    }
+    return () => {
+      _setUnmounted(true);
+    };
+  }, arr);
+}
