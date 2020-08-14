75156d7658665b2b7e4abe7a09f4e587250fa300
Ali Özkan <aliozzkan@gmail.com>
2020-08-14 17:06:16 +0300

    Required Setup And Create Env For React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./styles/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Technical Test</h1>
+      <h2>For Immfly</h2>
+      <p>By Ali Ozzkan</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
