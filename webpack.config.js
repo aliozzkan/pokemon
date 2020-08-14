a102fb8709c482b6bf41670a922048fea750bcd2
Ali Özkan <aliozzkan@gmail.com>
2020-08-15 01:45:41 +0300

    Babel Polyfill Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+require('babel-polyfill');
 
 module.exports = {
-  entry: './src/App.js',
+  entry: ['babel-polyfill', './src/App.js'],
   output: {
     path: path.join(__dirname, 'public'),
     publicPath: '/',
